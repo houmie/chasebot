218c05a464e59c07b3e7cdbd449b63b8492f43f4
H0um1e <none@none>
2012-07-17 09:59:01 +0100

    Fixes to the Aptana Django Project (Python Path)

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Chasebot.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chasebot.settings")
 
     from django.core.management import execute_from_command_line
 
