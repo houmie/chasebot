7fc901a1ae7678020fdefd8a4c2863a31d655edb
H0um1e <none@none>
2012-08-10 19:24:06 +0100

    working unit tests

--- chasebot_app/tests.py
+++ chasebot_app/tests.py
@@ -19,30 +19,54 @@ class SimpleTest(TestCase):
 
 
 
-#To test the data visibility of a contact model belonging to a single company1
+#To test the data visibility of a contact model belonging to a single company at a time
 class ContactModelTest(TestCase):
     def setUp(self):
         user1 = User.objects.create_user(username='username1', password='password1', email='email1@email.com')
         company1 = Company.objects.create(company_name='company_name1', company_email = 'company_email1@email.com')
-        userProfile1 = UserProfile(user=user1, company = company1)
-        userProfile1.save()
+        UserProfile.objects.create(user=user1, company = company1)
         
         user2 = User.objects.create_user(username='username2', password='password2', email='email2@email.com')
         company2 = Company.objects.create(company_name='company_name2', company_email = 'company_email2@email.com')
-        userProfile2 = UserProfile(user=user2, company = company2)
-        userProfile2.save()
-
-
+        UserProfile.objects.create(user=user2, company = company2)
+        
         ctype = ContactType.objects.create(contact_type= "test1")
                 
-        Contact.objects.create(last_name='last_name1', contact_type=ctype, company=company1)               
+        Contact.objects.create(last_name='last_name1a', contact_type=ctype, company=company1)               
+        Contact.objects.create(last_name='last_name1b', contact_type=ctype, company=company1)
         Contact.objects.create(last_name='last_name2', contact_type=ctype, company=company2)
         
-    def test_get_contacts_user1(self):
-        
+    def test_get_contacts_user1(self):        
         user1 = User.objects.get(username='username1')
         profile1 = user1.get_profile()        
         contacts = profile1.company.contact_set.all()
         
-        self.assertEqual(contacts.count(), 1, 'Expected one contact for this company, but got more')
-        self.assertEqual(contacts[0].last_name, 'last_name1')
+        self.assertEqual(contacts.count(), 2, 'Expected two contacts for this company, but got %s' % contacts.count())
+        self.assertEqual(contacts[0].last_name, 'last_name1b', 'Expected lastname1b, but got %s' % contacts[0].last_name)
+        self.assertEqual(contacts[1].last_name, 'last_name1a', 'Expected lastname1a, but got %s' % contacts[1].last_name)
+        
+
+class SalesItemModelTest(TestCase):
+    def setUp(self):
+        user1 = User.objects.create_user(username='username1', password='password1', email='email1@email.com')
+        company1 = Company.objects.create(company_name='company_name1', company_email = 'company_email1@email.com')
+        UserProfile.objects.create(user=user1, company = company1)
+        
+        user2 = User.objects.create_user(username='username2', password='password2', email='email2@email.com')
+        company2 = Company.objects.create(company_name='company_name2', company_email = 'company_email2@email.com')
+        UserProfile.objects.create(user=user2, company = company2)
+        
+        ctype = ContactType.objects.create(contact_type= "test1")
+                
+        Contact.objects.create(last_name='last_name1a', contact_type=ctype, company=company1)               
+        Contact.objects.create(last_name='last_name1b', contact_type=ctype, company=company1)
+        Contact.objects.create(last_name='last_name2', contact_type=ctype, company=company2)
+        
+    def test_get_contacts_user1(self):        
+        user1 = User.objects.get(username='username1')
+        profile1 = user1.get_profile()        
+        contacts = profile1.company.contact_set.all()
+        
+        self.assertEqual(contacts.count(), 2, 'Expected two contacts for this company, but got %s' % contacts.count())
+        self.assertEqual(contacts[0].last_name, 'last_name1b', 'Expected lastname1b, but got %s' % contacts[0].last_name)
+        self.assertEqual(contacts[1].last_name, 'last_name1a', 'Expected lastname1a, but got %s' % contacts[1].last_name)
